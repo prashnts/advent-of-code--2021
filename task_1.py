6dd6082686e926d171f92e8a14f1c826a5a26df7
Prashant Sinha <prashant.sinha@partoo.fr>
2021-12-02 14:59:49 +0100

    Task 1 and 2

---
+++ task_1.py
@@ -0,0 +1,36 @@
+TEST_DATA = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263,
+]
+
+def n_measurements_increasing(measurements):
+    pairs = zip(measurements, measurements[1:])
+    is_increasing = lambda x, y: x < y
+    return [is_increasing(*p) for p in pairs].count(True)
+
+
+if __name__ == '__main__':
+    assert n_measurements_increasing(TEST_DATA) == 7
+
+    with open('./task_1.input.txt', 'r') as fp:
+        lines = fp.read().split()
+        measurements = [int(x) for x in lines]
+
+    answer_1 = n_measurements_increasing(measurements)
+
+    measurements_2 = [
+        sum(x) for x in zip(measurements, measurements[1:], measurements[2:])
+    ]
+
+    answer_2 = n_measurements_increasing(measurements_2)
+
+    print(f'{answer_1=}')
+    print(f'{answer_2=}')
---
+++ task_2.py
@@ -0,0 +1,50 @@
+TEST_DATA = [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2",
+]
+
+def parse_command(cmd):
+    verb, amount_s = cmd.split(' ')
+    return verb, int(amount_s)
+
+def calculate_1(cmds):
+    hpos, depth = 0, 0
+    for cmd, amount in map(parse_command, cmds):
+        if cmd == 'forward':
+            hpos += amount
+        if cmd == 'down':
+            depth += amount
+        if cmd == 'up':
+            depth -= amount
+    return hpos * depth
+
+def calculate_2(cmds):
+    hpos, depth, aim = 0, 0, 0
+
+    for cmd, amount in map(parse_command, cmds):
+        if cmd == 'forward':
+            hpos += amount
+            depth += (aim * amount)
+        if cmd == 'down':
+            aim += amount
+        if cmd == 'up':
+            aim -= amount
+    return hpos * depth
+
+
+if __name__ == '__main__':
+    assert calculate_1(TEST_DATA) == 150
+    assert calculate_2(TEST_DATA) == 900
+
+    with open('./task_2.input.txt', 'r') as fp:
+        commands = fp.read().split('\n')
+
+    answer_1 = calculate_1(commands)
+    answer_2 = calculate_2(commands)
+
+    print(f'{answer_1=}')
+    print(f'{answer_2=}')
