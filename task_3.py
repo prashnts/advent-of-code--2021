c6b0564644b3f3a6804e68e698284df9d52ae6e4
Prashant Sinha <prashant@noop.pw>
2021-12-03 22:57:36 +0100

    day 3 -- kinda ugly solution

--- task_3.py
+++ task_3.py
@@ -22,6 +22,10 @@ def common_bits(bits):
     '''
     n_1 = bits.count('1')
     n_0 = bits.count('0')
+
+    if n_1 == n_0:
+        return ('1', '0')
+
     return ('1' if n_1 > n_0 else '0', '0' if n_1 > n_0 else '1')
 
 def calculate_1(data):
@@ -37,12 +41,46 @@ def calculate_1(data):
     return int(gamma, base=2) * int(epsilon, base=2)
 
 
+def calculate_2(data):
+    ox_gen_rating, co2_scrub_rating = '', ''
+    width = len(data[0])
+
+    measurements_ox = data[:]
+
+    for pos in range(width):
+        stack_ox = list(zip(*measurements_ox))[pos]
+        mcb, _ = common_bits(stack_ox)
+
+        measurements_ox = [measurements_ox[i] for i, value in enumerate(stack_ox) if value == mcb]
+
+        if len(measurements_ox) == 1:
+            ox_gen_rating = measurements_ox[0]
+            break
+
+    measurements_co2 = data[:]
+
+    for pos in range(width):
+        stack_co2 = list(zip(*measurements_co2))[pos]
+        _, lcb = common_bits(stack_co2)
+
+        measurements_co2 = [measurements_co2[i] for i, value in enumerate(stack_co2) if value == lcb]
+
+        if len(measurements_co2) == 1:
+            co2_scrub_rating = measurements_co2[0]
+            break
+
+    return int(ox_gen_rating, base=2) * int(co2_scrub_rating, base=2)
+
+
 if __name__ == '__main__':
     assert calculate_1(TEST_DATA) == 198
+    assert calculate_2(TEST_DATA) == 230
 
     with open('./task_3.input.txt', 'r') as fp:
         data = fp.read().split('\n')
 
     answer_1 = calculate_1(data)
+    answer_2 = calculate_2(data)
 
     print(f'{answer_1=}')
+    print(f'{answer_2=}')
